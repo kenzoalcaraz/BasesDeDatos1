87125ff892c35feee31c854cb748a63cbe6373ed
kenzoalcaraz <alcaraz.lauterio.k.mec@gmail.com>
2025-08-15 16:42:48 -0700

    Tarea 1

--- src/main/java/Tarea/ManejoDeArchivosDeDatos.java
+++ src/main/java/Tarea/ManejoDeArchivosDeDatos.java
@@ -1,8 +1,41 @@
 package Tarea;
 //15-ago-25
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class ManejoDeArchivosDeDatos {
     public static void main(String[] args) {
-        System.out.println("Código postal: - Número de asentamientos: ");
-    }
-}
+        FileReader fr = null;
+
+        try (java.io.FileReader fileReader = fr = new FileReader("codigos_postales_hmo.csv")){
+        BufferedReader br = new BufferedReader(fr);
+        String line = null;
+
+        String cpString = null;
+        int contador=1;
+        int lastCP = 0;
+
+        while ( (line = br.readLine()) != null ){
+
+            cpString = line.substring(0,5);
+            currentCP= Integer.parseInt(cpString);
+            if(lastCP == currentCP){
+                contador++;
+            }else {
+                System.out.println("%d $d\n", lastCP, contador);
+                contador = 1;
+            }
+        }
+        if( lastCP ==currentCP ){
+            System.out.println("%d $d\n", lastCP, contador);
+        }
+
+    }catch (FileNotFoundException e){
+            throw new RuntimeException(e);
+        }catch (IOException e){
+            throw new RuntimeException(e);
+        }
+}}
